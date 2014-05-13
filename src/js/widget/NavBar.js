38b213a83109f62deb7895583cbd5b7e6eb38c5f
Tom Wayson <tom@tomwayson.com>
2014-05-13 08:14:52 -0700

    added about modal dialog

--- src/js/widget/NavBar.js
+++ src/js/widget/NavBar.js
@@ -3,9 +3,15 @@ define([
   'dojo/query',
   'dojo/touch',
   'dojo/topic',
+
   'dijit/_WidgetBase',
   'dijit/_TemplatedMixin',
-  'dojo/text!./templates/NavBar.html'
+
+  'dojo/text!./templates/NavBar.html',
+
+  'bootstrap/Collapse',
+  'bootstrap/Dropdown',
+  'bootstrap/Modal'
 ], function(declare, query, touch, topic, _WidgetBase, _TemplatedMixin, template) {
   return declare([_WidgetBase, _TemplatedMixin], {
       templateString: template,
@@ -17,17 +23,28 @@ define([
 
       _attachEventHandlers: function() {
         var _this = this;
+        // change basemap
         query('.basemap-list li', this.domNode).on(touch.press, function(e) {
           e.preventDefault();
           topic.publish('basemap/set', {
             basemap: e.target.text
           });
-          // hide nav dropdown on mobile
-          if (query('.navbar-collapse.in', _this.domNode).length > 0) {
-            e.stopPropagation();
-            query('.navbar-toggle', _this.domNode)[0].click();
-          }
+          _this._hideDropdownNav(e);
         });
+        // show about modal
+        query('a[href="#about"]', this.domNode).on(touch.press, function(e) {
+          e.preventDefault();
+          query('.about-modal').modal('show');;
+          _this._hideDropdownNav(e);
+        });
+      },
+
+      _hideDropdownNav: function(e) {
+        // hide nav dropdown on mobile
+        if (query('.navbar-collapse.in', this.domNode).length > 0) {
+          e.stopPropagation();
+          query('.navbar-toggle', this.domNode)[0].click();
+        }
       }
    });
 });
