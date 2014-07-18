2c397960f0c2ffa89bb7474855fcec0cece1ca30
arnaud ferrand <arferrand@gmail.com>
2014-07-18 15:24:04 +0200

    adds ITA locale

---
+++ src/js/nls/it/strings.js
@@ -0,0 +1,10 @@
+define({
+
+    appTitle: 'Carta Dojo Bootstrap',
+    navBasemaps: 'Mappe di base',
+    navAbout: 'Circa',
+    modalAboutTitle: 'Circa',
+    modalAboutContent: 'Lo scopo di questa applicazione è quello di dimostrare come creare un\'applicazione con il miglior catographique Dojo (moduli AMD, classi e widget, promesse, i18n, rotte, ecc) tutti con un\'interfaccia utente reattiva Bootstrap.',
+    modalAboutMoreInfo: 'Seguito...'
+
+});
--- src/js/nls/strings.js
+++ src/js/nls/strings.js
@@ -7,6 +7,7 @@ define({
     modalAboutContent: 'The goal of this application boilerplate is to demonstrate how to build a mapping application that utilizes the best parts of Dojo (AMD modules, classes and widgets, promises, i18n, routing, etc) along with the responsive UI of Bootstrap.',
     modalAboutMoreInfo: 'More...'
   }),
-  fr: true
+  fr: true,
+  it: true
   // TODO: define other locales
 });
