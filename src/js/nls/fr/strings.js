e664e4f24790ba580aaa5e7e5c56efa5d2dd5e0a
arnaud ferrand <arferrand@gmail.com>
2014-07-22 09:06:29 +0200

    fr

--- src/js/nls/fr/strings.js
+++ src/js/nls/fr/strings.js
@@ -3,6 +3,6 @@ define({
     navBasemaps: 'Fonds de plan',
     navAbout: 'A propos',
     modalAboutTitle: 'A propos',
-    modalAboutContent: 'Le but de cette application est de montrer comment construire une application cartographique avec le meilleur de Dojo (modules AMD, classes et widgets, promesses, i18n, itinéraires, etc) tout cela avec une IHM réactive de Bootstrap.',
+    modalAboutContent: 'Le but de cette application est de montrer comment construire une application cartographique avec le meilleur de Dojo (modules AMD, classes et widgets, Promises, i18n, itinéraires, etc) tout cela avec une IHM réactive de Bootstrap.',
     modalAboutMoreInfo: 'Suite...'
 });
