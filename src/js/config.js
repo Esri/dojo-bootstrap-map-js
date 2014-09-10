d2eebdf448f6d73cf1ea93d2dd7f8f7c21a60c47
arnaud ferrand <arferrand@gmail.com>
2014-09-10 18:50:52 +0200

    config with localhost ampleworld

--- src/js/config.js
+++ src/js/config.js
@@ -13,38 +13,40 @@ define(['esri/InfoTemplate', ], function(InfoTemplate) {
       // TODO: add operationallayers
       operationalLayers: [{
         type: 'feature',
-        url: 'http://services1.arcgis.com/g2TonOxuRkIqSOFx/arcgis/rest/services/MeetUpHomeTowns/FeatureServer/0',
-        title: 'STLJS Meetup Home Towns',
+        url: 'http://sampleserver3.arcgisonline.com/ArcGIS/rest/services/Earthquakes/Since_1970/MapServer/0',
+        title: 'Earthquakes around the world',
         options: {
-          id: 'meetupHometowns',
+          id: 'earthquake',
           opacity: 1.0,
           visible: true,
           outFields: ['*'],
-          infoTemplate: new InfoTemplate('Hometown', '${*}'),
-          mode: 0
-        }
-      }, {
-        type: 'feature',
-        url: 'http://localhost:6080/arcgis/rest/services/SampleWorldCities/MapServer/0',
-        title: 'Cities',
-        options: {
-          id: 'city',
-          opacity: 1.0,
-          visible: true,
-          outFields: ['*'],
-          infoTemplate: new InfoTemplate('City', '${*}'),
+          infoTemplate: new InfoTemplate('Earthquake', '${*}'),
           mode: 0
         }
       }, {
         type: 'dynamic',
-        url: 'http://sampleserver6.arcgisonline.com/arcgis/rest/services/DamageAssessment/MapServer',
-        title: 'Damage Assessment',
+        url: 'http://sampleserver6.arcgisonline.com/arcgis/rest/services/SF311/MapServer',
+        title: 'SF311',
         options: {
-          id: 'DamageAssessment',
+          id: 'sf311',
           opacity: 0.5,
           visible: true
         }
-      }],
+      }
+      // ,{
+      //   type: 'feature',
+      //   url: 'http://localhost:6080/arcgis/rest/services/SampleWorldCities/MapServer/0',
+      //   title: 'Cities',
+      //   options: {
+      //     id: 'city',
+      //     opacity: 1.0,
+      //     visible: true,
+      //     outFields: ['*'],
+      //     infoTemplate: new InfoTemplate('City', '${*}'),
+      //     mode: 0
+      //   }
+      // }
+      ],
     },
     about: {
       moreInfoUrl: 'https://github.com/Esri/dojo-bootstrap-map-js'
