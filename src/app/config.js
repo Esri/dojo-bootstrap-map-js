e33507a05717a8e8bc0debfe64bcbb95c63097c1
Tom Wayson <tom@tomwayson.com>
2014-12-23 15:39:07 -0800

    map widgets are now configurable
    will only show on map if config settings supplied in config.js
    also added comments on map.id and map.options config settings
    resolves #56

--- src/app/config.js
+++ src/app/config.js
@@ -1,16 +1,24 @@
 define(['esri/InfoTemplate'], function(InfoTemplate) {
   return {
     map: {
-      //id: '3fdacfb3d72c4bd1a5e74a6730a955cc',
+      // if id is set, map will load from a webmap
+      // comment this out if you want to load layers from config below
       id: '4778fee6371d4e83a22786029f30c7e1',
-      //
+
+      // NOTE: if using a webmap (e.g. via id)
+      // this is the options sent to arcgisUtils.createMap()
+      // see: https://developers.arcgis.com/javascript/jsapi/esri.arcgis.utils-amd.html#createmap
+      // otherwise it is the options sent to new Map()
+      // see: https://developers.arcgis.com/javascript/jsapi/map-amd.html#map1
       options: {
+        scrollWheelZoom: true
+      },
       // uncomment these options if you do not use a webmap id
+      // options: {
       //    basemap: 'gray',
       //    center: [-117.1, 33.6],
       //    zoom: 9,
-          scrollWheelZoom: true
-      },
+      // }
       //
       // uncomment these section if you do not use a webmap id
       // operationalLayers: [{
@@ -47,9 +55,33 @@ define(['esri/InfoTemplate'], function(InfoTemplate) {
       //     mode: 0
       //   }
       // }],
-      // 
+      //
       // TODO: add basemaps
-      basemaps: {}      
+      // basemaps: {},
+
+      // Add config parameters for each map widget you want to include
+      // The map reference will get appended to the options
+      // To accept default options just pass empty object {}
+      // NOTE: to change the position of these widgets, make changes in map.css
+      widgets: {
+        scalebar: {
+          // see https://developers.arcgis.com/javascript/jsapi/scalebar-amd.html#scalebar1
+        },
+        homeButton: {
+          // see: https://developers.arcgis.com/javascript/jsapi/homebutton-amd.html#homebutton1
+        },
+        locateButton: {
+          // see: https://developers.arcgis.com/javascript/jsapi/locatebutton-amd.html#locatebutton1
+        },
+        geocoder: {
+          // see https://developers.arcgis.com/javascript/jsapi/geocoder-amd.html#geocoder1
+          autoComplete: true,
+          arcgisGeocoder: {
+            placeholder: 'Address or Location'
+          },
+          'class': 'geocoder'
+        }
+      }
     },
     about: {
       moreInfoUrl: 'https://github.com/Esri/dojo-bootstrap-map-js'
